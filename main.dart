47d8009097adad7efb4eb889966100721ece379f
Bhavik Makwana <bhavikmakwana43@gmail.com>
2019-05-04 19:20:38 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,4 @@
+void main() {
+  print("For the community by the community");
+  print("Flutter + Dart Tips repository is the collection of awesome tips that can help you during Flutter/Dart development.");
+}
